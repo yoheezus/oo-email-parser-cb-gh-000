97250d240480dd7e97b2e1d4a72582a2438a0f14
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-13 00:02:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,22 +4,17 @@
 # or whitespace (' ').
 class EmailParser
 
-    @@emails = []
-
     attr_accessor :emails
 
     def initialize(emails)
         @emails = emails
-
     end
 
     def parse
-        split_white_space = @emails.split(" ")
-        remove_commas = split_white_space.collect { |item| item.chomp(",")}
-        unique_emails = remove_commas.uniq
-        unique_emails.each { |email| @@emails << email }
-        unique_emails
-    end
+        emails.split.map do |email|
+              email.split(',')
+          end.flatten.uniq
+      end
 
     def self.all_emails
         @@emails
